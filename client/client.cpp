4d0d4575cb9bad53b8ac2874a134d1fbfa085957
Maowhob <m.y.maohoub@outlook.com>
2022-05-20 14:48:26 +0200

    update client, server and uml

--- client/client.cpp
+++ client/client.cpp
@@ -1,48 +1,43 @@
-
-
-#include <vsomeip/vsomeip.hpp>
 #include <cstdio>
 
-#define LOG_INF(...) fprintf(stdout, __VA_ARGS__), fprintf(stdout, "\n")
-#define LOG_ERR(...) fprintf(stderr, __VA_ARGS__), fprintf(stderr, "\n")
+#include <vsomeip/vsomeip.hpp>
 
 
 static vsomeip::service_t service_id = 0x1111;
 static vsomeip::instance_t service_instance_id = 0x2222;
 static vsomeip::method_t service_method_id = 0x3333;
 
-class hello_world_client {
+class ClientSender
+{
 public:
-    hello_world_client() :
-                    rtm_(vsomeip::runtime::get()),
-                    app_(rtm_->create_application("client"))
+    ClientSender() : rtm_(vsomeip::runtime::get()),
+                     app_(rtm_->create_application("client")){}
+
+    bool init()
     {
-
-    }
-
-    bool init(){
-        if (!app_->init()) {
-            LOG_ERR ("Couldn't initialize application");
+        if (!app_->init())
+        {
+            LOG_ERR("Couldn't initialize application");
             return false;
         }
 
         // register a state handler to get called back after registration at the
         // runtime was successful
         app_->register_state_handler(
-                std::bind(&hello_world_client::on_state_cbk, this,
-                        std::placeholders::_1));
+            std::bind(&ClientSender::on_state_cbk, this,
+                      std::placeholders::_1));
 
         // register a callback for responses from the service
         app_->register_message_handler(vsomeip::ANY_SERVICE,
-                service_instance_id, vsomeip::ANY_METHOD,
-                std::bind(&hello_world_client::on_message_cbk, this,
-                        std::placeholders::_1));
+                                       service_instance_id, vsomeip::ANY_METHOD,
+                                       std::bind(&ClientSender::on_message_cbk, this,
+                                                 std::placeholders::_1));
 
         // register a callback which is called as soon as the service is available
         app_->register_availability_handler(service_id, service_instance_id,
-                std::bind(&hello_world_client::on_availability_cbk, this,
-                        std::placeholders::_1, std::placeholders::_2,
-                        std::placeholders::_3));
+                                            std::bind(&ClientSender::on_availability_cbk, this,
+                                                      std::placeholders::_1, std::placeholders::_2,
+                                                      std::placeholders::_3));
         return true;
     }
 
@@ -55,7 +50,7 @@ public:
 
     void on_state_cbk(vsomeip::state_type_e _state)
     {
-        if(_state == vsomeip::state_type_e::ST_REGISTERED)
+        if (_state == vsomeip::state_type_e::ST_REGISTERED)
         {
             // we are registered at the runtime now we can request the service
             // and wait for the on_availability callback to be called
@@ -64,11 +59,10 @@ public:
     }
 
     void on_availability_cbk(vsomeip::service_t _service,
-            vsomeip::instance_t _instance, bool _is_available)
+                             vsomeip::instance_t _instance, bool _is_available)
     {
         // Check if the available service is the the hello world service
-        if(service_id == _service && service_instance_id == _instance
-                && _is_available)
+        if (service_id == _service && service_instance_id == _instance && _is_available)
         {
             // The service is available then we send the request
             // Create a new request
@@ -94,17 +88,13 @@ public:
 
     void on_message_cbk(const std::shared_ptr<vsomeip::message> &_response)
     {
-        if(service_id == _response->get_service()
-                && service_instance_id == _response->get_instance()
-                && vsomeip::message_type_e::MT_RESPONSE
-                        == _response->get_message_type()
-                && vsomeip::return_code_e::E_OK == _response->get_return_code())
+        if (service_id == _response->get_service() && service_instance_id == _response->get_instance() && vsomeip::message_type_e::MT_RESPONSE == _response->get_message_type() && vsomeip::return_code_e::E_OK == _response->get_return_code())
         {
             // Get the payload and print it
             std::shared_ptr<vsomeip::payload> pl = _response->get_payload();
             std::string resp = std::string(
-                    reinterpret_cast<const char*>(pl->get_data()), 0,
-                    pl->get_length());
+                reinterpret_cast<const char *>(pl->get_data()), 0,
+                pl->get_length());
             LOG_INF("Received: %s", resp.c_str());
             stop();
         }
@@ -116,7 +106,7 @@ public:
         app_->unregister_state_handler();
         // unregister the message handler
         app_->unregister_message_handler(vsomeip::ANY_SERVICE,
-                service_instance_id, vsomeip::ANY_METHOD);
+                                         service_instance_id, vsomeip::ANY_METHOD);
         // alternatively unregister all registered handlers at once
         app_->clear_all_handler();
         // release the service
@@ -130,38 +120,38 @@ private:
     std::shared_ptr<vsomeip::application> app_;
 };
 
-
-
 #ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
 #include <csignal>
 #endif
 
 #ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
-hello_world_client *hw_cl_ptr(nullptr);
-    void handle_signal(int _signal) {
-        if (hw_cl_ptr != nullptr &&
-                (_signal == SIGINT || _signal == SIGTERM))
-            hw_cl_ptr->stop();
-    }
-#endif
-#include<iostream>
-
-int main(int argc, char **argv)
+ClientSender *hw_cl_ptr(nullptr);
+void handle_signal(int _signal)
 {
-    (void)argc;
-    (void)argv;
+    if (hw_cl_ptr != nullptr &&
+        (_signal == SIGINT || _signal == SIGTERM))
+        hw_cl_ptr->stop();
+}
+#endif
+#include <iostream>
+
+int main(void)
+{
     std::cout << "Clinet Started ---------";
 
-    hello_world_client hw_cl;
+    ClientSender hw_cl;
 #ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
     hw_cl_ptr = &hw_cl;
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
 #endif
-    if (hw_cl.init()) {
+    if (hw_cl.init())
+    {
         hw_cl.start();
         return 0;
-    } else {
+    }
+    else
+    {
         return 1;
     }
 }
--- server/server.cpp
+++ server/server.cpp
@@ -1,48 +1,50 @@
-
+#include <cstdio>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
 
 #include <vsomeip/vsomeip.hpp>
-#include <cstdio>
-
-#define LOG_INF(...) fprintf(stdout, __VA_ARGS__), fprintf(stdout, "\n")
-#define LOG_ERR(...) fprintf(stderr, __VA_ARGS__), fprintf(stderr, "\n")
-
 
 static vsomeip::service_t service_id = 0x1111;
 static vsomeip::instance_t service_instance_id = 0x2222;
 static vsomeip::method_t service_method_id = 0x3333;
 
-class hello_world_client {
+class ServiceProvider
+{
 public:
-    hello_world_client() :
-                    rtm_(vsomeip::runtime::get()),
-                    app_(rtm_->create_application("client"))
+    ServiceProvider() : rtm_(vsomeip::runtime::get()),
+                        app_(rtm_->create_application()),
+                        stop_(false),
+                        stop_thread_(std::bind(&ServiceProvider::stop, this))
     {
-
     }
 
-    bool init(){
-        if (!app_->init()) {
-            LOG_ERR ("Couldn't initialize application");
+    ~ServiceProvider()
+    {
+        stop_thread_.join();
+    }
+
+    bool init()
+    {
+        // init the application
+        if (!app_->init())
+        {
+            LOG_ERR("Couldn't initialize application");
             return false;
         }
+
+        // register a message handler callback for messages sent to our service
+        app_->register_message_handler(service_id, service_instance_id,
+                                       service_method_id,
+                                       std::bind(&ServiceProvider::on_message_cbk, this,
+                                                 std::placeholders::_1));
 
         // register a state handler to get called back after registration at the
         // runtime was successful
         app_->register_state_handler(
-                std::bind(&hello_world_client::on_state_cbk, this,
-                        std::placeholders::_1));
-
-        // register a callback for responses from the service
-        app_->register_message_handler(vsomeip::ANY_SERVICE,
-                service_instance_id, vsomeip::ANY_METHOD,
-                std::bind(&hello_world_client::on_message_cbk, this,
-                        std::placeholders::_1));
-
-        // register a callback which is called as soon as the service is available
-        app_->register_availability_handler(service_id, service_instance_id,
-                std::bind(&hello_world_client::on_availability_cbk, this,
-                        std::placeholders::_1, std::placeholders::_2,
-                        std::placeholders::_3));
+            std::bind(&ServiceProvider::on_state_cbk, this,
+                      std::placeholders::_1));
         return true;
     }
 
@@ -53,115 +55,102 @@ public:
         app_->start();
     }
 
-    void on_state_cbk(vsomeip::state_type_e _state)
-    {
-        if(_state == vsomeip::state_type_e::ST_REGISTERED)
-        {
-            // we are registered at the runtime now we can request the service
-            // and wait for the on_availability callback to be called
-            app_->request_service(service_id, service_instance_id);
-        }
-    }
-
-    void on_availability_cbk(vsomeip::service_t _service,
-            vsomeip::instance_t _instance, bool _is_available)
-    {
-        // Check if the available service is the the hello world service
-        if(service_id == _service && service_instance_id == _instance
-                && _is_available)
-        {
-            // The service is available then we send the request
-            // Create a new request
-            std::shared_ptr<vsomeip::message> rq = rtm_->create_request();
-            // Set the hello world service as target of the request
-            rq->set_service(service_id);
-            rq->set_instance(service_instance_id);
-            rq->set_method(service_method_id);
-
-            // Create a payload which will be sent to the service
-            std::shared_ptr<vsomeip::payload> pl = rtm_->create_payload();
-            std::string str("World");
-            std::vector<vsomeip::byte_t> pl_data(std::begin(str), std::end(str));
-
-            pl->set_data(pl_data);
-            rq->set_payload(pl);
-            // Send the request to the service. Response will be delivered to the
-            // registered message handler
-            LOG_INF("Sending: %s", str.c_str());
-            app_->send(rq);
-        }
-    }
-
-    void on_message_cbk(const std::shared_ptr<vsomeip::message> &_response)
-    {
-        if(service_id == _response->get_service()
-                && service_instance_id == _response->get_instance()
-                && vsomeip::message_type_e::MT_RESPONSE
-                        == _response->get_message_type()
-                && vsomeip::return_code_e::E_OK == _response->get_return_code())
-        {
-            // Get the payload and print it
-            std::shared_ptr<vsomeip::payload> pl = _response->get_payload();
-            std::string resp = std::string(
-                    reinterpret_cast<const char*>(pl->get_data()), 0,
-                    pl->get_length());
-            LOG_INF("Received: %s", resp.c_str());
-            stop();
-        }
-    }
-
     void stop()
     {
+        std::unique_lock<std::mutex> its_lock(mutex_);
+        while (!stop_)
+        {
+            condition_.wait(its_lock);
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        // Stop offering the service
+        app_->stop_offer_service(service_id, service_instance_id);
         // unregister the state handler
         app_->unregister_state_handler();
         // unregister the message handler
-        app_->unregister_message_handler(vsomeip::ANY_SERVICE,
-                service_instance_id, vsomeip::ANY_METHOD);
-        // alternatively unregister all registered handlers at once
-        app_->clear_all_handler();
-        // release the service
-        app_->release_service(service_id, service_instance_id);
+        app_->unregister_message_handler(service_id, service_instance_id,
+                                         service_method_id);
         // shutdown the application
         app_->stop();
+    }
+
+    void terminate()
+    {
+        std::lock_guard<std::mutex> its_lock(mutex_);
+        stop_ = true;
+        condition_.notify_one();
+    }
+
+    void on_state_cbk(vsomeip::state_type_e _state)
+    {
+        if (_state == vsomeip::state_type_e::ST_REGISTERED)
+        {
+            // we are registered at the runtime and can offer our service
+            app_->offer_service(service_id, service_instance_id);
+        }
+    }
+
+    void on_message_cbk(const std::shared_ptr<vsomeip::message> &_request)
+    {
+
+        std::shared_ptr<vsomeip::message> resp = rtm_->create_response(_request);
+        std::string str("Server");
+        str.append(
+            reinterpret_cast<const char *>(_request->get_payload()->get_data()),
+            0, _request->get_payload()->get_length());
+
+        // Create a payload which will be sent back to the client
+        std::shared_ptr<vsomeip::payload> resp_pl = rtm_->create_payload();
+        std::vector<vsomeip::byte_t> pl_data(str.begin(), str.end());
+        resp_pl->set_data(pl_data);
+        resp->set_payload(resp_pl);
+
+        // Send the response back
+        app_->send(resp);
+        // we have finished
+        terminate();
     }
 
 private:
     std::shared_ptr<vsomeip::runtime> rtm_;
     std::shared_ptr<vsomeip::application> app_;
+    bool stop_;
+    std::mutex mutex_;
+    std::condition_variable condition_;
+    std::thread stop_thread_;
 };
-
-
 
 #ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
 #include <csignal>
 #endif
 
 #ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
-hello_world_client *hw_cl_ptr(nullptr);
-    void handle_signal(int _signal) {
-        if (hw_cl_ptr != nullptr &&
-                (_signal == SIGINT || _signal == SIGTERM))
-            hw_cl_ptr->stop();
-    }
-#endif
-#include<iostream>
-
-int main(int argc, char **argv)
+ServiceProvider *hw_srv_ptr(nullptr);
+void handle_signal(int _signal)
 {
-    (void)argc;
-    (void)argv;
-    std::cout << "Clinet Started ---------";
+    if (hw_srv_ptr != nullptr &&
+        (_signal == SIGINT || _signal == SIGTERM))
+        hw_srv_ptr->terminate();
+}
+#endif
+#include <iostream>
 
-    hello_world_client hw_cl;
+int main(void)
+{
+    std::cout << "Server started\n";
+    ServiceProvider hw_srv;
 #ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
-    hw_cl_ptr = &hw_cl;
+    hw_srv_ptr = &hw_srv;
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
 #endif
-    if (hw_cl.init()) {
-        hw_cl.start();
+    if (hw_srv.init())
+    {
+        hw_srv.start();
         return 0;
-    } else {
+    }
+    else
+    {
         return 1;
     }
 }
